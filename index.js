b300cdb545b373989f22e7d35895086451d4e425
sam9750 <sam.wadhwa95@gmail.com>
2022-01-15 01:33:09 -0500

    commit tree 7130f9f6fedac258141af7efcde3eb5ed749009b

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue){ 
+    return Math.abs(someValue - 42); 
+}
+
+function distanceFromHqInFeet(someValue) {
+distanceFromHqInBlocks(someValue);
+let distance = distanceFromHqInBlocks(someValue);
+    return distance * 264; 
+
+}
+
+
+
+    function distanceTravelledInFeet(start, destination) {
+        const block = 264
+        return Math.abs(start-destination) * 264; 
+
+
+    }
+
+
+
+
+    function calculatesFarePrice(start, destination) {
+        const travel = (start - destination) * 264
+        const travelmorethan2k = (Math.abs(start - destination)) * 264
+        if (travel < 400){
+            return 0;
+        }
+    else if (travel > 401 && travel < 2000) {
+        return (travel - 400) *0.02;
+    }
+    else if (travelmorethan2k > 2000 && travelmorethan2k < 2500) {
+    return 25;}}
